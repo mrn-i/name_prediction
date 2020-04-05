332c94a7c9aee4eb0fa20abb6621cde8456b6512
marine <marine.iuso@gmail.com>
2020-04-05 17:11:21 +0200

    corrected run.py for Production

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ import os
 if __name__ == "__main__":
 
 # in development
-    app.run(debug=True)
+    #app.run(debug=True)
 
 # before exporting
 # Bind to PORT if defined, otherwise default to 5000.
 
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
